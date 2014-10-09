eb28ca2df7a4e1e682fe64b263440046860c5b16
Pascal Cremer <b00gizm@gmail.com>
2014-10-09 15:46:29 +0200

    Initial commit

---
+++ puppet/manifests/main.pp
@@ -0,0 +1,24 @@
+Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+
+class system-update {
+  exec { 'apt-get update':
+    command => 'apt-get update',
+  }
+
+  $sysPackages = [ "curl", "ruby-dev", "nodejs" ]
+  package { $sysPackages:
+    ensure => "installed",
+    require => Exec['apt-get update'],
+  }
+}
+
+rvm_system_ruby {
+  'ruby-2.1.2':
+    ensure      => 'present',
+    default_use => true,
+}
+
+rvm::system_user { vagrant: ; }
+
+include system-update
+include rvm
